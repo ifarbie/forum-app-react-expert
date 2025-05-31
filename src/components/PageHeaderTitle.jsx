21aa2469b165afa512c1b0d5211c5483fdcde553
Fariz Rifky Berliano <farizrifkyberliano@gmail.com>
2025-05-31 07:35:50 +0700

    feat: add styled components package

---
+++ src/components/PageHeaderTitle.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const PageHeaderTitle = styled.h2.attrs({
+    className: "text-center font-medium text-3xl mb-6",
+})``;
+
+export default PageHeaderTitle;
